75a6a9c28d567e08e2da752c1fc1742e0cd169dd
taehyunL <taylor1233@naver.com>
2021-12-12 15:10:40 +0900

    initial commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by 이태현 on 2021/12/11.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
