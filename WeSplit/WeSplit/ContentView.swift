8b12d7fd453146759e281dcd5d0b3d638e8cc701
taehyunL <taylor1233@naver.com>
2021-12-12 15:11:00 +0900

    Picker 와 ForEach의 활용

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by 이태현 on 2021/12/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let name = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(name, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
