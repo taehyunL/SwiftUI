bc03e12b852f9f69cc8dcccb73a332e3224053c4
taehyunL <taylor1233@naver.com>
2021-12-14 14:54:50 +0900

    initial commit

---
+++ UnitConversion/UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by 이태현 on 2021/12/14.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
