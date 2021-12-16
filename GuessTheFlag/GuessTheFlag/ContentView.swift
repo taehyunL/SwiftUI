9ed144bf30fa56b230e102b7ea1a0cfc12880628
taehyunL <taylor1233@naver.com>
2021-12-16 16:27:39 +0900

    color 설정

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,22 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack {
-                Text("1")
-                Text("2")
-                Text("3")
+        ZStack {
+            VStack(spacing: 0) {
+                Color.red
+                Color.blue
             }
-            HStack {
-                Text("4")
-                Text("5")
-                Text("6")
-            }
-            HStack {
-                Text("7")
-                Text("8")
-                Text("9")
-            }
+            Text("Your Content")
+                .foregroundColor(.secondary)
+                .padding()
+                .background(.ultraThinMaterial)
         }
     }
 }
