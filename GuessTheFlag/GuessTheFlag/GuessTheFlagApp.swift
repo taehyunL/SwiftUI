b8ee330d0c9219e1168b01258fbb2188bb6cb585
taehyunL <taylor1233@naver.com>
2021-12-16 15:36:28 +0900

    initial commit

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by 이태현 on 2021/12/16.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
