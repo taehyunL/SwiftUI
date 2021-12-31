e5187e3a187a63ad1bc169908a5c860477d60445
taehyunL <taylor1233@naver.com>
2021-12-31 16:33:35 +0900

    initial commit

---
+++ R_P_S/R_P_S/R_P_SApp.swift
@@ -0,0 +1,17 @@
+//
+//  R_P_SApp.swift
+//  R_P_S
+//
+//  Created by 이태현 on 2021/12/31.
+//
+
+import SwiftUI
+
+@main
+struct R_P_SApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
