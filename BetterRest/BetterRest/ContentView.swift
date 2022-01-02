2f34d13a4bad8b5f9baf032963bb9c9e4940b07f
taehyunL <taylor1233@naver.com>
2022-01-02 19:29:35 +0900

    DatePicker 설정

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
     
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        DatePicker("please enter the date", selection: $wakeUp, in: Date.now...)
     }
 }
 
