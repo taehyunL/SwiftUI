1449ef2bc14b23db0a59261e64087a8a92c91c6b
taehyunL <taylor1233@naver.com>
2022-01-02 19:06:07 +0900

    initial commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by 이태현 on 2022/01/02.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
