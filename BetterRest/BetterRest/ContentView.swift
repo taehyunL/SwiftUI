6787759d014379bd9df2708da949b7349b616208
taehyunL <taylor1233@naver.com>
2022-01-03 21:39:09 +0900

    질문을 위한 View 구성

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var date = Date.now
+    @State var sleepAmount = 8.0
+    @State var coffeeAmount = 1
     
     var body: some View {
-        Text("Hello World!")
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter the time", selection: $date, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12)
+                
+                Text("Daliy coffee intake")
+                    .font(.headline)
+                Stepper(coffeeAmount == 1 ? "1Cup" : "\(coffeeAmount)Cups", value: $coffeeAmount, in: 1...20)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedTime)
+            }
+        }
+    }
+    
+    func calculateBedTime() {
+        
     }
 }
 
