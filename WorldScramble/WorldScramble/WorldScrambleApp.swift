ffea3ec5e6d1bc314825ea5f87f96e49ef6c1b0a
taehyunL <taylor1233@naver.com>
2022-01-06 12:50:27 +0900

    initial commit

---
+++ WorldScramble/WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by 이태현 on 2022/01/06.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
