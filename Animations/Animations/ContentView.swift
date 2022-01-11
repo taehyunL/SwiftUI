c16853216c9582d75dc4ba9a87bfcb0458a8cef0
taehyunL <taylor1233@naver.com>
2022-01-11 20:10:20 +0900

    rotation

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,29 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var animationAmount = 1.0
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        Button("tap me") {
-            animationAmount += 1
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
+            }
         }
         .padding(50)
-        .background(Color.red)
+        .background(.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
-        }
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
     }
 }
 
