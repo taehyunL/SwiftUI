59e9d6d1b1efe3532eeb850d1a9fd9ccf55b0e54
taehyunL <taylor1233@naver.com>
2022-01-11 17:59:04 +0900

    initial commit

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by 이태현 on 2022/01/11.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
