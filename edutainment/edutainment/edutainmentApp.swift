cd14787285ba7d1b01db9817058aa5d74bb54ab4
taehyunL <taylor1233@naver.com>
2022-01-19 17:44:57 +0900

    initial commit

---
+++ edutainment/edutainment/edutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  edutainmentApp.swift
+//  edutainment
+//
+//  Created by 이태현 on 2022/01/19.
+//
+
+import SwiftUI
+
+@main
+struct edutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
