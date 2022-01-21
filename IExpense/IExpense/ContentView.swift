ef308688f7499f8a472483c09dd7587d373d20cc
taehyunL <taylor1233@naver.com>
2022-01-21 19:32:48 +0900

    initial commit

---
+++ IExpense/IExpense/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  IExpense
+//
+//  Created by 이태현 on 2022/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IExpense/IExpense/IExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IExpenseApp.swift
+//  IExpense
+//
+//  Created by 이태현 on 2022/01/21.
+//
+
+import SwiftUI
+
+@main
+struct IExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
