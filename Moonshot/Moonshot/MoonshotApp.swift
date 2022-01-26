068ec2eb76fa3a8b23e1ca45b63e014393d1d1f1
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-01-26 18:17:29 +0900

    initial commit

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by 이태현 on 2022/01/26.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
