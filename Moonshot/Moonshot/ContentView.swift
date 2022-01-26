cd7162f3dbc2a33cf1952a180c16d852b6971d94
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-01-26 20:39:55 +0900

    복잡한 JSON형태 디코딩

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            VStack(spacing: 10) {
-                ForEach(0..<100) {
-                    Text("row is \($0)")
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
                 }
             }
-            .frame(maxWidth: .infinity)
+            """
+            let data = Data(input.utf8)
+            
+            if let user = try? JSONDecoder().decode(User.self, from: data) {
+                print(user.address.street)
+            }
+
         }
     }
 }
