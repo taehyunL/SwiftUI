cc726e07eb1d306cd1616eedf1f8ec13150df750
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-01-31 14:27:45 +0900

    astronauts json 파일 타입

---
+++ Moonshot/Moonshot/Astronauts.swift
@@ -0,0 +1,14 @@
+//
+//  Astronauts.swift
+//  Moonshot
+//
+//  Created by 이태현 on 2022/01/31.
+//
+
+import Foundation
+
+struct Astronauts: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
