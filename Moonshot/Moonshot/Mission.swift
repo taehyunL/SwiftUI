91ce911b1a5eff1e96a14e54bc1a5fb908007bb3
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-01-31 14:47:15 +0900

    Mission json파일의 형태

---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,21 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by 이태현 on 2022/01/31.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
