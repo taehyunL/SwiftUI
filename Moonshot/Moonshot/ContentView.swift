ca5e2506c86f3a33d385ddbd682c505f087e7772
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-01 13:12:51 +0900

    view 수정

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -11,9 +11,48 @@ struct ContentView: View {
     let astronauts: [String: Astronauts] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        Text("\(astronauts.count), \(missions.count)")
-            .padding()
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(RoundedRectangle(cornerRadius: 10)
+                                        .stroke()
+                            )
+                        }
+                    }
+                }
+                .padding([.leading, .bottom, .trailing])
+            }
+            .navigationTitle("Moonshoot")
+            .background(.darkBackground)
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
