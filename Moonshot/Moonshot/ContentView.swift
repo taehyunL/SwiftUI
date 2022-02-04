4da726fa6764821ce93f2a98076df4d81301c01c
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-04 20:19:03 +0900

    view 전환 위한 바인딩 변수 설정

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -10,49 +10,15 @@ import SwiftUI
 struct ContentView: View {
     let astronauts: [String: Astronauts] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
-    
-    let columns = [
-        GridItem(.adaptive(minimum: 150))
-    ]
+
+    @State var showingGrid: Bool = true
     
     var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                    Text(mission.formattedLaunchDate)
-                                        .font(.caption)
-                                        .foregroundColor(.white.opacity(0.5))
-                                }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
-                            }
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .overlay(RoundedRectangle(cornerRadius: 10)
-                                        .stroke()
-                            )
-                        }
-                    }
-                }
-                .padding([.leading, .bottom, .trailing])
-            }
-            .navigationTitle("Moonshoot")
-            .background(.darkBackground)
+        if showingGrid {
+            GridView(astronauts: astronauts, missions: missions, showingGrid: $showingGrid)
+        } else {
+            ListView(astronauts: astronauts, missions: missions, showingGrid: $showingGrid)
         }
-        .preferredColorScheme(.dark)
     }
 }
 
