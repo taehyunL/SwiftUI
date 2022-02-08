dea94f78e9af9fb0b93be48a478c1bb0a61c1be9
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-08 19:43:12 +0900

    Arc를 그리는 방법, 위치 각도에 주의

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,16 +7,39 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.closeSubpath()
+        
+        return path
+    }
+}
+
+struct Arc: Shape {
+    let startAngle: Angle
+    let endAngel: Angle
+    let clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle - Angle.degrees(90), endAngle: endAngel - Angle.degrees(90), clockwise: !clockwise)
+        
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Path { path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 200))
-            path.addLine(to: CGPoint(x: 300, y: 200))
-            path.addLine(to: CGPoint(x: 200, y: 100))
-            path.closeSubpath()
-        }
-        .stroke(.blue, lineWidth: 3)
+        Arc(startAngle: Angle.degrees(0), endAngel: Angle.degrees(110), clockwise: true)
+            .stroke(.blue, lineWidth: 10)
+            .frame(width: 100, height: 100)
     }
 }
 
