71a0cbe93ddac240653118d8e56a86884e621ad1
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-09 18:45:44 +0900

    CGAffineTransform을 이용한 그리기

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,45 +7,37 @@
 
 import SwiftUI
 
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-        path.closeSubpath()
-        
-        return path
-    }
-}
+struct Flower: Shape {
+    var petalOffset: Double = -20
 
-struct Arc: InsettableShape {
-    let startAngle: Angle
-    let endAngle: Angle
-    let clockwise: Bool
-    var insetAmount = 0.0
-    
+    var petalWidth: Double = 100
+
     func path(in rect: CGRect) -> Path {
-        
         var path = Path()
-        
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: startAngle - Angle.degrees(90), endAngle: endAngle - Angle.degrees(90), clockwise: !clockwise)
-        
+
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number)
+
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+
+            let rotatedPetal = originalPetal.applying(position)
+
+            path.addPath(rotatedPetal)
+        }
+
         return path
-    }
-    
-    func inset(by amount: CGFloat) -> some InsettableShape {
-        var arc = self
-        arc.insetAmount += amount
-        return arc
     }
 }
 
 struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+
     var body: some View {
-        Arc(startAngle: Angle.degrees(-90), endAngle: Angle.degrees(90), clockwise: true)
-            .strokeBorder(.blue, lineWidth: 40)
+        Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+            .fill(.red, style: FillStyle(eoFill: true))
     }
 }
 
