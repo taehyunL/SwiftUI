542729159dc5017a5ba554e056d585b4322a5643
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-11 23:25:19 +0900

    크기에 맞는 circle

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,37 +7,37 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    var petalOffset: Double = -20
-
-    var petalWidth: Double = 100
-
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            let rotation = CGAffineTransform(rotationAngle: number)
-
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-
-            let rotatedPetal = originalPetal.applying(position)
-
-            path.addPath(rotatedPetal)
-        }
-
-        return path
-    }
-}
-
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    @State private var amount = 0.0
 
     var body: some View {
-        Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-            .fill(.red, style: FillStyle(eoFill: true))
+        VStack {
+            ZStack {
+                Circle()
+                    .fill(.red)
+                    .frame(width: 200 * amount)
+                    .offset(x: -50, y: -80)
+                    .blendMode(.screen)
+
+                Circle()
+                    .fill(.green)
+                    .frame(width: 200 * amount)
+                    .offset(x: 50, y: -80)
+                    .blendMode(.screen)
+
+                Circle()
+                    .fill(.blue)
+                    .frame(width: 200 * amount)
+                    .blendMode(.screen)
+            }
+            .frame(width: 300, height: 300)
+
+            Slider(value: $amount)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .ignoresSafeArea()
     }
 }
 
