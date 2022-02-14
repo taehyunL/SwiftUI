6d6b49db33fbcdf159a460db7036b619552d4501
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-14 18:23:07 +0900

    initial commit

---
+++ HabitTrack/HabitTrack/HabitTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackApp.swift
+//  HabitTrack
+//
+//  Created by 이태현 on 2022/02/14.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
