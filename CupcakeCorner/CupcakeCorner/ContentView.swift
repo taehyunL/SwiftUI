bd15fb700076f7f3c7ec3f96252b227bfa4b4be5
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-17 15:28:15 +0900

    @Published 프로프티를 codable 형식에 따르게  만들기

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by 이태현 on 2022/02/17.
+//
+
+import SwiftUI
+
+class User: ObservableObject, Codable {
+    
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    @Published var name = "Paul Hudson"
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
