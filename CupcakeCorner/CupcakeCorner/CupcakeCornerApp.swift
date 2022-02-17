1639a638f179159ca256949a71e26203c44a2a8e
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-17 15:27:48 +0900

    initial commit

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by 이태현 on 2022/02/17.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
