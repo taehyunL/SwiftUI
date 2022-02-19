37fa12ffa214dbea850f25f3c52a992dd3ba4129
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-19 22:58:51 +0900

    disable 통한 활성화/비활성화

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,23 +7,32 @@
 
 import SwiftUI
 
-struct ContentView: View {
     
+struct ContentView: View {
+    @State private var username = ""
+    @State private var email = ""
+
     var body: some View {
-        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
-            if let image = phase.image {
-                image
-                    .resizable()
-                    .scaledToFit()
-            } else if phase.error != nil {
-                Text("There was an error loading the image")
-            } else {
-                ProgressView()
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
+
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
+                }
+            }
+            .disabled(disableForm)
         }
-        .frame(width: 200, height: 200)
+    }
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
