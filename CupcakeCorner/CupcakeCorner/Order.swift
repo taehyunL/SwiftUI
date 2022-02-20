4a5dd09a663f7cebc6a547f9dc0aae44067118d0
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-20 21:20:11 +0900

    view 전체에서의 data model 사용을 위한 order 클래스

---
+++ CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  CupcakeCorner
+//
+//  Created by 이태현 on 2022/02/20.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnable = false {
+        didSet {
+            if !specialRequestEnable {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+    
+}
