a8eb2553dd3b30fb9fc53e0509dc78c7c040d443
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-24 00:05:49 +0900

    @Binding 활용 양방향 연결

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -7,10 +7,30 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct PushButton: View {
+    var title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [ Color.red, Color.yellow ]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+        
+    }
+    
+}
+
+struct ContentView: View {
+    @State var rememberMe = false
+    
+    var body: some View {
+        PushButton(title: "remember", isOn: $rememberMe)
+        Text( rememberMe ? "True" : "False")
     }
 }
 
