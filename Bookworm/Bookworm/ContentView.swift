d459b407547cbacb91a69f0d9d5c3c17c20b16a0
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-24 01:49:22 +0900

    managedObjectContext의 활용

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -7,30 +7,29 @@
 
 import SwiftUI
 
-struct PushButton: View {
-    var title: String
-    @Binding var isOn: Bool
-    
-    var onColors = [ Color.red, Color.yellow ]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-    
-    var body: some View {
-        Button(title) {
-            isOn.toggle()
-        }
-        .padding()
-        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
-        
-    }
-    
-}
-
 struct ContentView: View {
-    @State var rememberMe = false
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
     
     var body: some View {
-        PushButton(title: "remember", isOn: $rememberMe)
-        Text( rememberMe ? "True" : "False")
+        VStack {
+            List(students) { student in
+                Text(student.name ?? "unknown")
+            }
+            Button("Add") {
+                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastNames.randomElement()!
+
+                let student = Student(context: moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                
+                try? moc.save()
+            }
+        }
     }
 }
 
