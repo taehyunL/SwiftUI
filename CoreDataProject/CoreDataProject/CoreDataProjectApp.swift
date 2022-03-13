bdd05a9528009cbbfede351169d3c90f243ead31
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-03-13 17:44:56 +0900

    environment 설정

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by 이태현 on 2022/03/13.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    @StateObject private var dataController = DataController()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+        }
+    }
+}
