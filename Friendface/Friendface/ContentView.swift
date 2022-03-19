b872c37aec18521ba9887c22dcaf27c48582c4a3
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-03-19 20:52:42 +0900

    initial commit

---
+++ Friendface/Friendface/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Friendface
+//
+//  Created by 이태현 on 2022/03/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Friendface/Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by 이태현 on 2022/03/19.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
