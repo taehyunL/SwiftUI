33cda793925fecb599b0bd3662ef6098a1421ca8
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-03-24 18:04:09 +0900

    initial commit

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by 이태현 on 2022/03/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
