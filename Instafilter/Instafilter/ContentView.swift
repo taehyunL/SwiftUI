b3bbf3ea27ea347c2e6c1fe945ffb5893383bd72
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-03-31 19:58:43 +0900

    swiftui만의 기능으로는 부족했는 image 다루기를 UIKit활용

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -4,28 +4,41 @@
 //
 //  Created by 이태현 on 2022/03/24.
 //
-
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
 
     var body: some View {
-        Text("Hello, World!")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                showingConfirmation = true
-            }
-            .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                Button("Red") { backgroundColor = .red }
-                Button("Green") { backgroundColor = .green }
-                Button("Blue") { backgroundColor = .blue }
-                Button("Cancel", role: .cancel) { }
-            } message: {
-                Text("Select a new color")
-            }
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+
+    func loadImage() {
+        guard let inputImage = UIImage(named: "Example") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.sepiaTone()
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 1
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        // attempt to get a CGImage from our CIImage
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            // convert that to a UIImage
+            let uiImage = UIImage(cgImage: cgimg)
+
+            // and convert that to a SwiftUI image
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
