2c4532d1cafdba9abef7459fc9abbd83ae392d2f
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-04-24 18:50:18 +0900

    UIKit 과 SwiftUI의 연동을 통한 갤러리에서 사진 가져오기

--- Instafilter/Instafilter/ImagePicker.swift
+++ Instafilter/Instafilter/ImagePicker.swift
@@ -9,16 +9,44 @@ import PhotosUI
 import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?//바인딩을 설정하여 변경사항을 보낼 수 있음
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            guard let provider = results.first?.itemProvider else { return }
+            
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }//didFinishPicking은 갤러리에서 취소를 누르거나 사진을 선택했을때
+    }
+    
+    
     func makeUIViewController(context: Context) -> PHPickerViewController {
         var config = PHPickerConfiguration()
         config.filter = .images
         
         let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator // viewController 와 SwiftUI 의 다리의 역할을 하는 delegate
         return picker
     }
     
     func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
         
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
     
     typealias UIViewControllerType = PHPickerViewController
